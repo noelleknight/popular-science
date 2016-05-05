9251e1faa7748f9d84d4f5ed952d70f8d7e3785e
Noelle Knight <noelle.knight@gmail.com>
2016-05-05 14:59:17 -0400

    Controller test fails successfully

--- test/specs/create-new-author.controller.js
+++ test/specs/create-new-author.controller.js
@@ -1,9 +1,10 @@
 (function() {
   'use strict';
-
+var assert = chai.assert;
 
   suite('create new author controller', function() {
     var $rootScope;
+    var createNewAuthorController;
     var mockNewAuthorService = {};
     var mockLoginService = {};
 
@@ -45,14 +46,19 @@
       };
     }));
     test('newAuthorForm errors correctly', function (doneCallback) {
+      // what the user might type into the form
       createNewAuthorController.newAuthor = {
         name: 'foobar'
       };
-      doneCallback();
+      // now they click submit... and it fires...
+      createNewAuthorController.newAuthorForm()
+        .then(function(){
+          console.log('in test then, do we have a messge?');
+          assert.ok(createNewAuthorController.errorMessage, "Error message exists on failure");
+          doneCallback();
+        });
+        $rootScope.$digest();
     });
-    $rootScope.$digest();
   });
-
-});
 
 })();
